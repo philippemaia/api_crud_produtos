1d616ce44fe5a796b9a8a284d1ea5efe98dbb29b
philippemaia <philippe.m.maia@hotmail.com>
2023-06-17 03:21:29 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/api_crud_produtos/ApiCrudProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_crud_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudProdutosApplication.class, args);
+	}
+
+}
