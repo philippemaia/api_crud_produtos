f94a13cbec92c7875f2626c4bd2f9d24ee37d178
philippemaia <philippe.m.maia@hotmail.com>
2023-06-18 03:36:51 -0300

    Adicionando o endpoint de listar produtos

--- src/main/java/com/example/api_crud_produtos/controllers/ProdutoController.java
+++ src/main/java/com/example/api_crud_produtos/controllers/ProdutoController.java
@@ -1,2 +1,16 @@
-package com.example.api_crud_produtos.controllers;public class ProdutoController {
+package com.example.api_crud_produtos.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoController {
+
+    @GetMapping
+    public ResponseEntity listarTodosProdutos(){
+        return ResponseEntity.ok("OK");
+    }
 }
