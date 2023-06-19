81f0da626d284f1a66808ba1286ad2ae1d4be150
philippemaia <philippe.m.maia@hotmail.com>
2023-06-18 23:20:09 -0300

    Inclusão de comportamentos de cadastar e listar produtos na aplicação

--- src/main/java/com/example/api_crud_produtos/controllers/ProdutoController.java
+++ src/main/java/com/example/api_crud_produtos/controllers/ProdutoController.java
@@ -1,11 +1,14 @@
 package com.example.api_crud_produtos.controllers;
 
+import com.example.api_crud_produtos.dto.ProdutoDto;
+import com.example.api_crud_produtos.entities.Produto;
 import com.example.api_crud_produtos.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/produtos")
@@ -18,5 +21,16 @@ public class ProdutoController {
     public ResponseEntity listarTodosProdutos(){
         var produtos = repository.findAll();
         return ResponseEntity.ok(produtos);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity cadastrarProduto(@RequestBody @Valid ProdutoDto dto, UriComponentsBuilder uriBuilder){
+        Produto produto = new Produto(dto);
+        repository.save(produto);
+
+        var uri = uriBuilder.path("/produtos/{id}").buildAndExpand(produto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new Produto(dto));
     }
 }
